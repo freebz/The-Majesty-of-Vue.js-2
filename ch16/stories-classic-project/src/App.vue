8442ef36dac92e7dbf9e3f52ccaf4b576834b7e3
FreeBz <freebz@hananet.net>
2018-02-23 11:33:32 +0900

    16장 단일 파일 컴포넌트

---
+++ ch16/stories-classic-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <login></login> -->
+    <!-- <router-view/> -->
+    <register></register>
+    <!-- <stories></stories> -->
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+import Register from './components/Register'
+import Stories from './components/Stories.vue'
+  
+export default {
+  name: 'app',
+  components: {
+    Login,
+    Register,
+    Stories
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
